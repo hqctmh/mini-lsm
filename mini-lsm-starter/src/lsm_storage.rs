4a732a281f58ca9c44960b1c23b363bb2303795b
mahao <shanqiu.mh@raycloud.com>
2026-02-17 18:11:43 +0800

    week1 day1 task2

--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -298,7 +298,18 @@ impl LsmStorageInner {
 
     /// Get a key from the storage. In day 7, this can be further optimized by using a bloom filter.
     pub fn get(&self, _key: &[u8]) -> Result<Option<Bytes>> {
-        unimplemented!()
+        let guard = self.state.read();
+        let option = guard.memtable.get(_key);
+        if option.is_none() {
+            Ok(option)
+        } else {
+            let bytes = option.unwrap();
+            if (bytes.is_empty()) {
+                Ok(None)
+            } else {
+                Ok(Some(bytes))
+            }
+        }
     }
 
     /// Write a batch of data into the storage. Implement in week 2 day 7.
@@ -308,12 +319,14 @@ impl LsmStorageInner {
 
     /// Put a key-value pair into the storage by writing into the current memtable.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        let guard = self.state.read();
+        guard.memtable.put(_key, _value)
     }
 
     /// Remove a key from the storage by writing an empty value.
     pub fn delete(&self, _key: &[u8]) -> Result<()> {
-        unimplemented!()
+        let guard = self.state.read();
+        guard.memtable.put(_key, &[])
     }
 
     pub(crate) fn path_of_sst_static(path: impl AsRef<Path>, id: usize) -> PathBuf {
